a762023f2ad6f87eeab69e284894a09d4b1b4ecf
Santana20 <sstfmscontreras@gmail.com>
2022-04-26 20:45:40 -0500

    comentado la funcionalidad de deteccion de fecha en texto

--- index.py
+++ index.py
@@ -137,39 +137,39 @@ def predict_activity(activity_text):
 
 ### Deteccion de fecha
 ################################################
-from sutime import SUTime
-import string
-#sutime = SUTime(mark_time_ranges=True, include_range=True,language='spanish')
-sutime = SUTime(language='spanish')
-dias_semana = [ 'lunes', 'martes', 'miércoles','miercoles', 'jueves', 'viernes', 'sabado', 'sábado', 'domingo' ]
-meses = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','setiembre','septiembre','octubre','noviembre','diciembre']
+# from sutime import SUTime
+# import string
+# #sutime = SUTime(mark_time_ranges=True, include_range=True,language='spanish')
+# sutime = SUTime(language='spanish')
+# dias_semana = [ 'lunes', 'martes', 'miércoles','miercoles', 'jueves', 'viernes', 'sabado', 'sábado', 'domingo' ]
+# meses = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','setiembre','septiembre','octubre','noviembre','diciembre']
 
-def get_date_by_points(arr_dates):
-    maxi, maxpoints = -1, 0
-    for index in range(len(arr_dates)):
-        obj_date = arr_dates[index]
+# def get_date_by_points(arr_dates):
+#     maxi, maxpoints = -1, 0
+#     for index in range(len(arr_dates)):
+#         obj_date = arr_dates[index]
 
-        ## preprocesamiento del texto
-        text_date = obj_date['text'].lower().strip()
-        for c in (string.punctuation + ' \n'):
-            text_date = text_date.replace(c, " ")
-        text_date = text_date.split(' ')
+#         ## preprocesamiento del texto
+#         text_date = obj_date['text'].lower().strip()
+#         for c in (string.punctuation + ' \n'):
+#             text_date = text_date.replace(c, " ")
+#         text_date = text_date.split(' ')
         
-        ## Puntaje para la fecha
-        points = 0
-        for word in text_date:
-            if word.isdigit():
-                points += 1
-            elif word in (dias_semana + meses):
-                points += 2
+#         ## Puntaje para la fecha
+#         points = 0
+#         for word in text_date:
+#             if word.isdigit():
+#                 points += 1
+#             elif word in (dias_semana + meses):
+#                 points += 2
 
-        if points > maxpoints:
-            maxi = index
-            maxpoints = points
+#         if points > maxpoints:
+#             maxi = index
+#             maxpoints = points
     
-    if maxi == -1:
-        return None
-    return arr_dates[maxi]['value']
+#     if maxi == -1:
+#         return None
+#     return arr_dates[maxi]['value']
 
 
 ################################################
@@ -186,16 +186,16 @@ def registrar_actividad():
             desc_actividad = request.form['desc_actividad']
             variable = predict_activity(desc_actividad)[0]
 
-            arr_dates = sutime.parse(desc_actividad.lower())
-            fecha = get_date_by_points(arr_dates)
-
-            #fecha = request.form.get('fecha')
-            #check_fecha = request.form.get('check_fecha')
+            fecha = request.form.get('fecha')
+            check_fecha = request.form.get('check_fecha')
 
             print(nom_actividad, desc_actividad)
             print('variable:',variable)
-            print('posibles fechas\n', sutime.parse(desc_actividad))
-            print(fecha)
+
+            # arr_dates = sutime.parse(desc_actividad.lower())
+            # fecha = get_date_by_points(arr_dates)
+            # print('posibles fechas\n', sutime.parse(desc_actividad))
+            # print(fecha)
 
             cur.execute("""
                     INSERT INTO `actividades` 
